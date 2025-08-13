a5a397338b6a6c0490e91ddf75c8fb15aeb2786f
Akhil-Patil-Bagili <akhilpatilbagili@gmail.com>
2025-08-13 00:02:24 -0700

    chore(contracts): setup schema + codegen + build

---
+++ codegen.ts
@@ -0,0 +1,12 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'src/schema/index.graphql',
+  generates: {
+    'generated/schema-types.ts': {
+      plugins: ['typescript', 'typescript-operations', 'typescript-resolvers']
+    }
+  }
+};
+
+export default config;

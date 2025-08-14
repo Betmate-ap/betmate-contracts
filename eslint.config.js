ffd3bd4aadb6672036ccf60e70c38639aa31b4ae
Akhil Patil Bagili <akhilpatilbagili@gmail.com>
2025-08-14 01:25:50 -0700

    chore: align lint, prettier, type-check with contracts repo (#2)
    * chore: align lint, prettier, type-check with contracts repo
    
    * upgrading eslint to v9
    
    * chore: ignore generated code; run Prettier separately; simplify ESLint config
    
    * chore(contracts): format files with Prettier

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    // ignore outputs and generated code
+    ignores: ["node_modules", "dist", "coverage", "build", "generated"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    // you can add TypeScript-specific or project rules here
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+];
